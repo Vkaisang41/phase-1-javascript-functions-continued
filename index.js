fded82063f8eadc7709eb25fa550b95107cd5fa0
Vkaisang41 <kaisang.vincent@student.moringaschool.com>
2025-06-21 23:48:07 +0300

    Define functions: saturdayFun, mondayWork, wrapAdjective

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// code your solution here
+// 1. Function declaration for saturdayFun
+function saturdayFun(activity = "roller-skate") {
+  return `This Saturday, I want to ${activity}!`;
+}
+
+// 2. Function expression for mondayWork
+const mondayWork = function(activity = "go to the office") {
+  return `This Monday, I will ${activity}.`;
+}
+
+// 3. Function that returns another function
+function wrapAdjective(flair = "*") {
+  return function(adjective = "special") {
+    return `You are ${flair}${adjective}${flair}!`;
+  };
+}
+
